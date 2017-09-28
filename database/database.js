46e45d57824916f38e7b9a8061b912d08000f747
student <hyungjon.kim@gmail.com>
2017-09-28 09:14:13 +0800

    Database now does not add new value if it is equal to latest value

--- database/database.js
+++ database/database.js
@@ -15,7 +15,12 @@ function Database() {
   this.insert = function(key, val, timestamp) {
     var version = {key:key, value:val, timestamp:timestamp};
     if (this.data[key]) {
-      this.data[key].unshift(version);
+      // design choice: if received value is already latest value, do nothing, for time and storage efficiency (but server still returns timestamp of POST request)
+      // this is because this app is a version control and does not necessarily need to remember POST requests that make no difference to the database, and because this behavior in DB makes no observable difference outside this module
+      // however, if it is decided that keeping track of when all POST requests were received is also important, then this function can be omitted
+      if (this.data[key][0].value !== val) {
+        this.data[key].unshift(version);
+      }
     } else {
       this.data[key] = [version];
     }
@@ -38,7 +43,7 @@ function Database() {
   };
 
   // find the latest version that was added before or at given timestamp
-  // assumption: if given timestamp is before earliest version, return empty object since the key did not exist in DB at that time
+  // assumption: if given timestamp is before earliest version, return null since the key did not exist in DB at that time
   // assumption: if given timestamp is after latest version, return latest value since it will be the value at given timestamp as long as the value is not updated before that
   this.getLatestBeforeTimestamp = function(key, timestamp, callback) {
     if (!this.data[key]) return callback({error:"Key not found"});
